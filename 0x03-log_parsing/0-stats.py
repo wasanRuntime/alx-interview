b2e38ee2774a7e2adbd11f41a77859524d0cc22f
wasanRuntime <andrewwasan@gmail.com>
2024-03-29 00:15:50 +0300

    generate

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,54 +1,40 @@
 #!/usr/bin/python3
-
+"""Module containing script that reads stdin and computes metrics"""
 import sys
 
-
-def print_msg(dict_sc, total_file_size):
-    """
-    Method to print
-    Args:
-        dict_sc: dict of status codes
-        total_file_size: total of the file
-    Returns:
-        Nothing
-    """
-
-    print("File size: {}".format(total_file_size))
-    for key, val in sorted(dict_sc.items()):
-        if val != 0:
-            print("{}: {}".format(key, val))
-
-
-total_file_size = 0
-code = 0
-counter = 0
-dict_sc = {"200": 0,
-           "301": 0,
-           "400": 0,
-           "401": 0,
-           "403": 0,
-           "404": 0,
-           "405": 0,
-           "500": 0}
+status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
+                "403": 0, "404": 0, "405": 0, "500": 0}
+total_size = 0
+total_num = 0
 
 try:
     for line in sys.stdin:
-        parsed_line = line.split()  # ✄ trimming
-        parsed_line = parsed_line[::-1]  # inverting
+        lines = line.split(" ")
 
-        if len(parsed_line) > 2:
-            counter += 1
+        if len(lines) > 4:
+            code = lines[-2]
+            size = int(lines[-1])
 
-            if counter <= 10:
-                total_file_size += int(parsed_line[0])  # file size
-                code = parsed_line[1]  # status code
+            if code in status_codes.keys():
+                status_codes[code] += 1
 
-                if (code in dict_sc.keys()):
-                    dict_sc[code] += 1
+            total_size += size
+            total_num += 1
 
-            if (counter == 10):
-                print_msg(dict_sc, total_file_size)
-                counter = 0
+        if total_num == 10:
+            total_num = 0
+            print("File size: {}".format(total_size))
+
+            for k, v in sorted(status_codes.items()):
+                if v != 0:
+                    print("{}: {}".format(k, v))
+
+except Exception:
+    pass
 
 finally:
-    print_msg(dict_sc, total_file_size)
+    print("File size: {}".format(total_size))
+
+    for k, v in sorted(status_codes.items()):
+        if v != 0:
+            print("{}: {}".format(k, v))
