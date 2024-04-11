97156aab46b3d5a465238663995e4767b547e6e8
wasanRuntime <andrewwasan@gmail.com>
2024-04-11 23:49:47 +0300

    up

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,44 +1,69 @@
 #!/usr/bin/python3
-""" N queens """
+"""Module with solution for the N-Queens challenge with backtracking"""
 import sys
 
 
-if len(sys.argv) > 2 or len(sys.argv) < 2:
+def chessboard(pos, n):
+    """Function to print chessboard with appropriate positions of queens"""
+
+    board = []
+
+    for RANK in range(n):
+        for FILE in range(n):
+            if FILE == pos[RANK]:
+                board.append([RANK, FILE])
+
+    print(board)
+
+
+def safe_pos(pos, RANK, FILE, n):
+    """Function to determine safe a square to place a queen"""
+
+    if (pos[RANK] == FILE) or (pos[RANK] == FILE - RANK + n) or \
+            (pos[RANK] == RANK - n + FILE):
+        return True
+    return False
+
+
+def get_safe_pos(board, rank, n):
+    """Function to get all safe positions to place queens with recursion"""
+
+    if rank == n:
+        chessboard(board, n)
+    else:
+        for FILE in range(n):
+            safe = True
+
+            for RANK in range(rank):
+                if safe_pos(board, RANK, FILE, rank):
+                    safe = False
+
+            if safe:
+                board[rank] = FILE
+                get_safe_pos(board, rank + 1, n)
+
+
+def create_board(n):
+    """Function to generate chessboard of n-size"""
+
+    return [0 * n for i in range(n)]
+
+
+if len(sys.argv) != 2:
     print("Usage: nqueens N")
     exit(1)
 
-if not sys.argv[1].isdigit():
+try:
+    n = int(sys.argv[1])
+except Exception:
     print("N must be a number")
     exit(1)
 
-if int(sys.argv[1]) < 4:
+if (n < 4):
     print("N must be at least 4")
     exit(1)
 
-n = int(sys.argv[1])
 
-
-def queens(n, i=0, a=[], b=[], c=[]):
-    """ find possible positions """
-    if i < n:
-        for j in range(n):
-            if j not in a and i + j not in b and i - j not in c:
-                yield from queens(n, i + 1, a + [j], b + [i + j], c + [i - j])
-    else:
-        yield a
-
-
-def solve(n):
-    """ solve """
-    k = []
-    i = 0
-    for solution in queens(n, 0):
-        for s in solution:
-            k.append([i, s])
-            i += 1
-        print(k)
-        k = []
-        i = 0
-
-
-solve(n)
+board = create_board(int(n))
+rank = 0
+get_safe_pos(board, rank, int(n))
